03956e17d596ee5b6f4da31b7ee0c5a52a34d271
ManuelPuenteMtz <manuel.puentemr@uanl.edu.mx>
2025-09-25 10:00:59 -0600

    Evidencia 10

---
+++ Evidencia 10/Evidencia10_validar_archivos.ps1
@@ -0,0 +1,33 @@
+﻿function Validar-Archivo {
+    param (
+        [string]$Ruta
+    )
+
+    # Generar nombre del reporte con fecha al final (formato ddMMyyyy)
+    $fechaArchivo = Get-Date -Format "ddMMyyyy"
+    $Reporte = ([Environment]::GetFolderPath("Desktop") + "\ReporteValidacion_$fechaArchivo.txt")
+
+    $resultado = ""
+
+    try {
+        if (Test-Path $Ruta) {
+            $contenido = Get-Content $Ruta -ErrorAction Stop
+            $resultado = "Archivo encontrado y accesible: $Ruta"
+        } else {
+            throw "El archivo no existe."
+        }
+    }
+    catch {
+        $resultado = "Error: $_"
+    }
+    finally {
+        Write-Host "Validación finalizada para: $Ruta" -ForegroundColor Cyan  
+
+        $fecha = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+        $linea = "[$fecha] $resultado"
+        Add-Content -Path $Reporte -Value $linea
+        Write-Host "Reporte guardado en: $Reporte" -ForegroundColor Green
+    }
+
+    return $resultado
+}
